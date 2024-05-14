94b4f80dbfd68fb6e5b71fa1e99bf4dfa4e9fb31
JustineFundal <128974973+JustineFundal@users.noreply.github.com>
2024-05-15 02:55:06 +0800

    assesment.sol

--- assesment.sol
+++ assesment.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-contract ErrorHandling {
+contract project {
     address public owner;
     uint public value;
 
@@ -13,26 +13,35 @@ contract ErrorHandling {
 
     function setValue(uint _value) public {
 
-        require(msg.sender == owner, "Caller is not the owner");
+        require(msg.sender == owner, "you are not the owner");
         
-        require(_value > 0, "Value must be greater than 0");
+        require(_value > 100, "Value must be greater than 100");
         
         value = _value;
     }
 
 
     function incrementValue(uint _increment) public {
+        assert(value >= _increment);
+       
         value += _increment;
         
-        assert(value >= _increment);
+        
     }
 
     function resetValue() public {
         if (msg.sender != owner) {
-            revert("Caller is not the owner");
+
+            revert("you are not the owner");
+
+        }
+        else {
+            value = 0;
         }
         
-        value = 0;
+        
+        
+
     }
 
     function getValue() public view returns (uint) {
